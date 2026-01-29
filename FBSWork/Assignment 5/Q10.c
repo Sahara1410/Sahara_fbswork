440eb09b49b3ef9274b56785c9127e1909758726
Sahara1410 <saharapathan3727@gmail.com>
2026-01-29 17:54:08 +0530

    Add files via upload

---
+++ FBSWork/Assignment 5/Q10.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main() {
+	int n=4;
+	for(  int i=1; i<=n; i++) {
+		for(int j=1; j<=4; j++) {
+			if(i==1||j==1||i==4||j==4||i==j)
+			printf("* ");
+			else
+				printf("  ");
+		}
+			printf("\n");
+	}
+}
---
+++ FBSWork/Assignment 5/Q8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int n=5;
+	int i,j;
+	for(int i=1;i<=n;i++){
+		for(int j=1; j<=i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+	for(int i=n;i>=1;i--){
+		for(int j=1; j<i;j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+}
+	
+	
