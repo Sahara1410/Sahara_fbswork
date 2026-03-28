8c7b03db7e6db116d44ed77d061fa44f9bdf91db
Sahara1410 <saharapathan3727@gmail.com>
2026-03-28 11:02:57 +0530

    Add files via upload

---
+++ FBSWork/C module test/String.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[]="Sahara Pathan";
+	printf("The string is:%s",str);
+	int count=0;
+	int i;
+	for(i=0;i<=str[i];i++)
+	{
+		count++;
+	}
+	printf("\nTotal no in string is:%d",count);
+}
